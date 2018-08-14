42ebebcd74f6acecc1d12d4708d42539f4fc7ea4
兴斋 <xingzhai.xxw@alibaba-inc.com>
2018-08-14 17:48:27 +0800

    feat: redux.

---
+++ src/types/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Copyright(c) Alibaba Group Holding Limited.
+ *
+ * 
+ * @Authors: 兴斋 <xingzhai.xxw@alibaba-inc.com> (https://www.gistop.com/)
+ * @Link   : https://www.gistop.com
+ * @Date   : 2018-8-14 17:46:01
+ */
+
+export interface StoreState {
+  languageName: string;
+  enthusiasmLevel: number;
+}
